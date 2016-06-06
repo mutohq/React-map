9114235dc986bbda1b029358ab7f5930a0c80caa
ashu01 <as01hu@gmail.com>
2016-06-06 16:15:36 +0530

    test case updated

--- jsfilestoparse/test.ios.js
+++ jsfilestoparse/test.ios.js
@@ -1,59 +1,45 @@
-return(
-<ActivityIndicatorIOS   >
-<DatePickerIOS>
-<DrawerLayoutAndroid>
-</Image   >
-</ListView >
-<MapView>
-<Modal>
-<Navigator>
-<NavigatorIOS>
-<PickerIOS>
-</Picker>
-<ProgressBarAndroid>
-<ProgressViewIOS>
-<RefreshControl>
-<ScrollView>
-<SegmentedControlIOS>
-<Slider>
-<SliderIOS>
-<StatusBar>
-</Switch>
-</TabBarIOS>
-<Text>
-</TextInput>
-<ToolbarAndroid>
-<TouchableHighlight>
-<TouchableNativeFeedback>
-<TouchableOpacity>
-<TouchableWithoutFeedback>
-<View>
-<ViewPagerAndroid>
-<WebView>
-<ProgressBarAndroid      > );
+return ( < ActivityIndicatorIOS >
+    < DatePickerIOS >
+    < DrawerLayoutAndroid >
+    < /Image   > < /ListView > < MapView >
+    < Modal >
+    < Navigator >
+    < NavigatorIOS >
+    < PickerIOS >
+    < / Picker> < ProgressBarAndroid >
+    < ProgressViewIOS >
+    < RefreshControl >
+    < ScrollView >
+    < SegmentedControlIOS >
+    < Slider >
+    < SliderIOS >
+    < StatusBar >
+    < /Switch> < /TabBarIOS> < Text >
+    < /TextInput> < ToolbarAndroid >
+    < TouchableHighlight >
+    < TouchableNativeFeedback >
+    < TouchableOpacity >
+    < TouchableWithoutFeedback >
+    < View >
+    < ViewPagerAndroid >
+    < WebView >
+    < ProgressBarAndroid > );
 
-return(
-<ActivityIndicatorIOS   >
-<DatePickerIOS>
-<DrawerLayoutAndroid>
-</Image   >
-</ListView >
-<MapView> 
-<Abc>
-</Det>
+return ( < ActivityIndicatorIOS >
+    < DatePickerIOS >
+    < DrawerLayoutAndroid >
+    < /Image   > < /ListView > < MapView >
+    < Abc >
+    < /Det>
 );
 
-return(
-<ActivityIndicatorIOS   >
-<DatePickerIOS>
-<DrawerLayoutAndroid>
-</Image   >
-</ListView >
-<MapView> 
-<Xys>
-<Axy>
+return ( < ActivityIndicatorIOS >
+    < DatePickerIOS >
+    < DrawerLayoutAndroid >
+    < /Image   > < /ListView > < MapView >
+    < Xys >
+    < Axy >
 );
-return(
-<View>
-</View>
+return ( < View >
+    < /View>
 );
