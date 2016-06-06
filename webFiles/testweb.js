e6bec74f37149ef8318601bdc1327d94cc7978d6
ashu01 <as01hu@gmail.com>
2016-06-06 16:13:55 +0530

    output file for any type of operation

---
+++ webFiles/testweb.js
@@ -0,0 +1,45 @@
+return (/********************************************************************************** < ActivityIndicatorIOS >
+    < DatePickerIOS >
+    < DrawerLayoutAndroid >
+    < /Image   > < /Listspan > < Mapspan >
+    < Modal >
+    < Navigator >
+    < NavigatorIOS >
+    < PickerIOS >
+    < / Picker> < div >
+    < ProgressspanIOS >
+    < RefreshControl >
+    < Scrollspan >
+    < SegmentedControlIOS >
+    < Slider >
+    < SliderIOS >
+    < StatusBar >
+    < /Switch> < /TabBarIOS> < p >
+    < /pInput> < ToolbarAndroid >
+    < TouchableHighlight >
+    < TouchableNativeFeedback >
+    < TouchableOpacity >
+    < TouchableWithoutFeedback >
+    < span >
+    < spanPagerAndroid >
+    < Webspan >
+    < div > **********************************************************************************/);
+
+return (/********************************************************************************** < ActivityIndicatorIOS >
+    < DatePickerIOS >
+    < DrawerLayoutAndroid >
+    < /Image   > < /Listspan > < Mapspan >
+    < Abc >
+    < /Det>
+**********************************************************************************/);
+
+return (/********************************************************************************** < ActivityIndicatorIOS >
+    < DatePickerIOS >
+    < DrawerLayoutAndroid >
+    < /Image   > < /Listspan > < Mapspan >
+    < Xys >
+    < Axy >
+**********************************************************************************/);
+return ( < span >
+    < /span>
+);
