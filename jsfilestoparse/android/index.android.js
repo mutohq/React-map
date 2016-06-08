6ea4ee01421f9d6dbd7aeaf0e47d212d0070df78
ashu01 <as01hu@gmail.com>
2016-06-08 14:41:25 +0530

    android output

--- jsfilestoparse/android/index.android.js
+++ jsfilestoparse/android/index.android.js
@@ -1,12 +1,13 @@
-
 // Import Statements
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  NavigatorIOS
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    NavigatorIOS
 } from 'react-native';
 
 // Include Any Pages
@@ -16,52 +17,51 @@ var SearchPage = require('./SearchPage');
 
 // Class App
 class HelloWorld extends Component {
-  render(){
-    return(
-      <View style={styles.container}>
-      <Text style={styles.name}>
-      Hello, Bhavyanth!
-      </Text>
-      </View>
-      
-      
+    render() {
+        return ( /*<View style = {styles.container}>
+            /*<Text style ={styles.name}>
+            Hello, Bhavyanth!
+            </Text>*/ </View>*/ 
+			/*<Nav />*/
+			/*<App />*/
 
-      );
-  }
+
+        );
+    }
 }
 
 class PropertyFinder extends Component {
-  render() {
-    return (
-      <NavigatorIOS style={styles.containerNav}
-      initialRoute={{
-        title: 'PropertyFinder',
-        component: SearchPage,
-      }}/>
-    );
-  }
+    render() {
+        return ( /*<NavigatorIOS style ={styles.containerNav
+            }
+            initialRoute = {{
+                    title: 'PropertyFinder',
+                    component: SearchPage,
+                }}/>*/
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  containerNav:{
-    flex: 1
-  },
-  name: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  location: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    containerNav: {
+        flex: 1
+    },
+    name: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    location: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
