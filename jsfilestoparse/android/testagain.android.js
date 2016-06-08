392020633004fc47dc1158972f069d2483fbb012
ashu01 <as01hu@gmail.com>
2016-06-08 16:08:47 +0530

    output android file

---
+++ jsfilestoparse/android/testagain.android.js
@@ -0,0 +1,66 @@
+// Import Statements
+import React, {
+    Component
+} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    NavigatorIOS
+} from 'react-native';
+
+// Include Any Pages
+var SearchPage = require('./SearchPage');
+
+
+
+// Class App
+class HelloWorld extends Component {
+    render() {
+        return ( /*<View style={styles.container}>
+            /*<Text style={styles.name}>
+            Hello, Bhavyanth!
+            </Text>*/ </View>*/*/
+			/*<TouchableHighlight style={styles.button}
+            onPress={this.onSearchPressed.bind(this)}
+            underlayColor='#99d9f4'>
+          /*<View style={styles.buttonp}>Go</View>*/*/
+        </TouchableHighlight>*/
+
+
+        );
+    }
+}
+
+class PropertyFinder extends Component {
+    render() {
+        return ( /*<View>
+            </View>*/
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    containerNav: {
+        flex: 1
+    },
+    name: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    location: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
